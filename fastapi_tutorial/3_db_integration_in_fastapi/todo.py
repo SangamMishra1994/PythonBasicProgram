8285aa997c50f7ac71f7c1416aba8ba8980fb085
sangammishra1994 <sangammishra001@gmail.com>
2026-02-02 23:25:22 +0530

    Add two API in this file.

--- fastapi_tutorial/3_db_integration_in_fastapi/todo.py
+++ fastapi_tutorial/3_db_integration_in_fastapi/todo.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, Path, Query, status
+from sqlmodel import select
 from models.Todo import Todo
 from request_model.TodoRequest import TodoRequest
 from response_model.TodoResponse import TodoResponse
@@ -23,14 +24,41 @@ async def hello():
 
 
 @app.post(
-    "/todo/create", response_model=TodoResponse,
-    status_code=status.HTTP_201_CREATED
+    "/todo/create", response_model=TodoResponse, status_code=status.HTTP_201_CREATED
 )
-async def create_todo(
-    todoReq: TodoRequest, session: SessionDependency
-):
+async def create_todo(todoReq: TodoRequest, session: SessionDependency):
     todo = Todo.model_validate(todoReq)
     session.add(todo)
     session.commit()
     session.refresh(todo)
     return todo
+
+
+@app.get("/todo/all", response_model=list[TodoResponse],
+         status_code=status.HTTP_200_OK
+         )
+async def get_all(session: SessionDependency):
+    query = select(Todo)
+    result = session.exec(query).all()
+    return result
+
+
+@app.get("/todo/priority", response_model=list[TodoResponse],
+         status_code=status.HTTP_200_OK)
+async def get_todo_by_priority(session: SessionDependency,
+                               priority: int = Query(ge=1, le=5)
+                               ):
+    query = select(Todo).where(Todo.priority == priority)
+    result = session.exec(query).all()
+    return result
+
+
+@app.get("/todo/{id}", response_model=TodoResponse)
+async def get_todo_by_id(session: SessionDependency, id: int = Path(ge=1)):
+    todo = session.get(Todo, id)
+    if not todo:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Id not found")
+    return todo
+
+
+
